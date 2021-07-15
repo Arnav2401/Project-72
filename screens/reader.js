1b5ad9f4e1b4cd96404b3899c5a211e1d3811a1d
Arnav2401 <77386547+Arnav2401@users.noreply.github.com>
2021-07-15 18:47:02 +0530

    Add files via upload

---
+++ screens/reader.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { View,Text } from "react-native";
+
+export default class Reader extends React.Component{
+render(){
+    return(
+        <View>
+            <Text style={{textAlign:'center',marginTop:30}}>This Feature is not available! Kindly wait till i complete my next class</Text>
+        </View>
+    )
+}
+}
